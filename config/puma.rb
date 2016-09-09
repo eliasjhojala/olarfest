4c02b8d3b7a721b88bb1e051ed9db3bb5ffd5b63
Roope Salmi <rpsalmi@gmail.com>
2016-09-09 23:43:34 +0300

    Correct deploy.sh

--- config/puma.rb
+++ config/puma.rb
@@ -1,12 +1,12 @@
 if ENV['ENVIRONMENT'] == 'production'
-  root = File.expand_path("..", Dir.pwd)
 
   # pumactl (DON'T allow external access!)
   activate_control_app "tcp://127.0.0.1:#{File.read('tmp/control_port').strip}"
 
-  bind "unix://#{root}/tmp/puma.sock"
-  pidfile "#{root}/tmp/puma.pid"
-  rackup "#{root}/config.ru"
-  state_path "#{root}/tmp/puma.state"
+  bind "unix://tmp/puma.sock"
+  pidfile "tmp/puma.pid"
+  rackup "config.ru"
+  state_path "tmp/puma.state"
+  stdout_redirect "tmp/out.log", "tmp/err.log"
   daemonize true
 end
--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,11 @@
+#!/bin/bash
+
 bundle install
-pumactl -F puma.rb restart
+export ENVIRONMENT=production
+bundle exec pumactl -F config/puma.rb status > /dev/null
+if [ $? -eq 0 ] # If puma is already running
+then
+  bundle exec pumactl -F config/puma.rb restart
+else
+  bundle exec pumactl -F config/puma.rb start
+fi
