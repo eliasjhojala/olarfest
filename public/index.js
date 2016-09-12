09946cf13098d21f6c5a4d3ff0165ea3d79bd7db
Roope Salmi <rpsalmi@gmail.com>
2016-09-12 18:27:48 +0300

    Move linkbar opacity handling back to index.js, don't use opacity when window is narrow

--- public/index.js
+++ public/index.js
@@ -1,5 +1,15 @@
 
+var topOpacity;
+var bottomOpacity;
+var linkbar, teaser;
+
 $( document ).ready(function() {
+  initLinkbarOpacity();
+  setOpacity();
+  $( window ).scroll(function() {
+    setOpacity();
+  });
+  
   $('video').bind("timeupdate", function() {
     if(this.currentTime >= 11) {
       this.pause();
@@ -32,3 +42,36 @@ $( document ).ready(function() {
     linkBarFlow();
   });
 });
+
+$( document ).ready(function() {
+});
+
+function initLinkbarOpacity() {
+  //calcuate variables from the html/css
+  linkbar = $('#linkbar');
+  teaser = $('#teaser');
+  topOpacity = linkbar.css("background-color");
+  topOpacity = Number(topOpacity.replace(/^.*,(.+)\)/,'$1'));
+  bottomOpacity = 1;
+  
+  console.log(linkbar.css("background-color"));
+}
+
+function setOpacity() {
+  function calculate() {
+    if ($(window).width() < 888) return 1;
+    var start = (teaser.height() / 1.4);
+    var current = teaser.height() - $(window).scrollTop() - linkbar.outerHeight();
+    var max = teaser.height() - linkbar.outerHeight();
+    var opacity = 1 - (current / max * (bottomOpacity - topOpacity));
+    
+    if (opacity > 1) {
+      opacity = 1;
+    }
+    if (opacity < topOpacity) {
+      opacity = topOpacity;
+    }
+    return opacity;
+  }
+  linkbar.css("background-color", "rgba(0,0,0,"+calculate()+")");
+}
