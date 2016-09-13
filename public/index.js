dec86cc7277639b77300a55680cfb5dca68ef82e
Elias Ojala <elias.jh.ojala@gmail.com>
2016-09-13 15:40:13 +0300

    little update to video system

--- public/index.js
+++ public/index.js
@@ -13,6 +13,10 @@ $( document ).ready(function() {
       
       alert("yli");
   }
+  else if($(window).width() > 600) {
+      $('#teaserVideo').html('<source src="teaser_LowRes.mp4" type="video/mp4">');
+      alert("alle");
+  }
   else {
       $('#teaserVideo').html('<source src="teaser_LowRes2.mp4" type="video/mp4">');
       alert("alle");
