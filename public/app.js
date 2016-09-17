4a672c07db317bca561a6fd95f15ca21fe2bd3b2
Roope Salmi <rpsalmi@gmail.com>
2016-09-17 12:00:51 +0300

    Make navigation bar -links use unobtrusive javascript

--- public/app.js
+++ public/app.js
@@ -1,1 +1,11 @@
 
+$(document).ready(function () {
+  $('#linkbar a').click(function(e) {
+    e.preventDefault();
+    var element = $(this).attr('href');
+    if (element !== "#") {
+      history.pushState({}, '', element);
+      scrollToElement(element);
+    }
+  });
+});
