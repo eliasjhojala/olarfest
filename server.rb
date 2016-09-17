cab317fde6d518390b9fccfbe7271480e1f8ed37
Elias Ojala <elias.jh.ojala@gmail.com>
2016-09-17 11:54:19 +0300

    rekryDescription update

--- server.rb
+++ server.rb
@@ -4,9 +4,15 @@ class OlarFest < Sinatra::Base
 	helpers Sinatra::ContentFor
 	
 	get '/' do
+		@rekryType = nil
 		erb :root
 	end
 	get '/rekryDescriptions/markkinointi' do
 		erb :'rekryDescriptions/markkinointi'
+	end
+	
+	get '/rekry/:type' do |type|
+	 @rekryType = type
+	 erb :root
 	end
 end
