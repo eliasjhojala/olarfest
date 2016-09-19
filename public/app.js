2a762422cff1111e4e18c876a0e1d460a00b0bd5
Elias Ojala <elias.jh.ojala@gmail.com>
2016-09-19 10:41:14 +0300

    Scroll speed is always the same

--- public/app.js
+++ public/app.js
@@ -1,7 +1,13 @@
 function scrollToElement(elementName) {
+  var scrollTop     = $('body').scrollTop(),
+    elementOffset = $(elementName).offset().top,
+    distance      = (elementOffset - scrollTop);
+    
+    distance = Math.min(Math.abs( distance ), 1500);
+    
   $('html, body').animate({
     scrollTop: $(elementName).offset().top - 100
-  }, 500);
+  }, distance);
 }
 
 
